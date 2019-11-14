a1809c2e6f696f89b9081a98fa6c7a45d8d7c7ce
yollaek <yusuf.ollaek@valeo.com>
2019-11-14 09:24:07 +0200

    fix design object or ErrorListView

--- src/Gemini.Modules.ErrorList/Design/DesignTimeErrorListViewModel.cs
+++ src/Gemini.Modules.ErrorList/Design/DesignTimeErrorListViewModel.cs
@@ -1,11 +1,70 @@
-﻿using Gemini.Modules.ErrorList.ViewModels;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Caliburn.Micro;
 
 namespace Gemini.Modules.ErrorList.Design
 {
-    public class DesignTimeErrorListViewModel : ErrorListViewModel
+    public class DesignTimeErrorListViewModel : PropertyChangedBase
     {
+        private IObservableCollection<ErrorListItem> _items;
+
+        public IObservableCollection<ErrorListItem> Items
+        {
+            get { return _items; }
+        }
+
+        public IEnumerable<ErrorListItem> FilteredItems
+        {
+            get
+            {
+                var items = _items.AsEnumerable();
+                if (!ShowErrors)
+                    items = items.Where(x => x.ItemType != ErrorListItemType.Error);
+                if (!ShowWarnings)
+                    items = items.Where(x => x.ItemType != ErrorListItemType.Warning);
+                if (!ShowMessages)
+                    items = items.Where(x => x.ItemType != ErrorListItemType.Message);
+                return items;
+            }
+        }
+        private bool _showErrors = true;
+        public bool ShowErrors
+        {
+            get { return _showErrors; }
+            set
+            {
+                _showErrors = value;
+                NotifyOfPropertyChange(() => ShowErrors);
+                NotifyOfPropertyChange("FilteredItems");
+            }
+        }
+
+        private bool _showWarnings = true;
+        public bool ShowWarnings
+        {
+            get { return _showWarnings; }
+            set
+            {
+                _showWarnings = value;
+                NotifyOfPropertyChange(() => ShowWarnings);
+                NotifyOfPropertyChange("FilteredItems");
+            }
+        }
+
+        private bool _showMessages = true;
+        public bool ShowMessages
+        {
+            get { return _showMessages; }
+            set
+            {
+                _showMessages = value;
+                NotifyOfPropertyChange(() => ShowMessages);
+                NotifyOfPropertyChange("FilteredItems");
+            }
+        }
         public DesignTimeErrorListViewModel()
         {
+            _items = new BindableCollection<ErrorListItem>();
             Items.Add(new ErrorListItem
             {
                 ItemType = ErrorListItemType.Error,
